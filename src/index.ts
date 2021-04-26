63281eeba9d43856286ad20fc48830aea6a097c4
Anna Vargová <64069511+AnickaVargova@users.noreply.github.com>
2021-04-26 17:04:25 +0200

    function main, connection.manager replaced by patternRepository

--- src/index.ts
+++ src/index.ts
@@ -6,57 +6,57 @@ import cors from "cors";
 import { Request, Response } from "express";
 import random from "./random";
 
-createConnection()
-  .then(async (connection) => {
-    const patternRepository = connection.getRepository(Pattern);
+async function main() {
+  const connection = await createConnection();
+  const app = express();
 
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
+  const patternRepository = connection.getRepository(Pattern);
 
-    app.get("/", async function (req: Request, res: Response) {
-      const results = await patternRepository.find();
-      return res.send(results.map((item) => item.name).concat("random"));
+  app.use(express.json());
+  app.use(cors());
+
+  app.get("/", async function (req: Request, res: Response) {
+    const results = await patternRepository.find();
+    return res.send(results.map((item) => item.name).concat("random"));
+  });
+
+  app.get("/random", (req, res) => res.send(random()));
+
+  app.get("/:setting", async function (req: Request, res: Response) {
+    const patternName = req.params.setting;
+    const results = await patternRepository.find();
+    return res.send(
+      results.find((item) => item.name === patternName).patternJSON
+    );
+  });
+
+  app.post("/:setting", async function (req: Request, res: Response) {
+    const patternName = req.params.setting;
+    const pattern = new Pattern();
+    pattern.name = patternName;
+    pattern.patternJSON = JSON.stringify(req.body);
+    const results = await patternRepository.save(pattern);
+
+    return res.send(results);
+  });
+
+  app.put("/:setting", async function (req: Request, res: Response) {
+    const patternName = req.params.setting;
+    const results = await patternRepository.update(
+      { name: patternName },
+      { patternJSON: JSON.stringify(req.body) }
+    );
+    return res.send(results);
+  });
+
+  app.delete("/:setting", async function (req: Request, res: Response) {
+    const patternName = req.params.setting;
+    const results = await patternRepository.delete({
+      name: patternName,
     });
+    return res.send(results);
+  });
+  app.listen(8080);
+}
 
-    app.get("/random", (req, res) => res.send(random()));
-
-    app.get("/:setting", async function (req: Request, res: Response) {
-      const patternName = req.params.setting;
-      const results = await patternRepository.find();
-      return res.send(
-        results.find((item) => item.name === patternName).patternJSON
-      );
-    });
-
-    app.post("/:setting", async function (req: Request, res: Response) {
-      const patternName = req.params.setting;
-      const pattern = new Pattern();
-      pattern.name = patternName;
-      pattern.patternJSON = JSON.stringify(req.body);
-
-      const results = await connection.manager.save(pattern);
-
-      return res.send(results);
-    });
-
-    app.put("/:setting", async function (req: Request, res: Response) {
-      const patternName = req.params.setting;
-      const results = await connection.manager.update(
-        Pattern,
-        { name: patternName },
-        { patternJSON: JSON.stringify(req.body) }
-      );
-      return res.send(results);
-    });
-
-    app.delete("/:setting", async function (req: Request, res: Response) {
-      const patternName = req.params.setting;
-      const results = await connection.manager.delete(Pattern, {
-        name: patternName,
-      });
-      return res.send(results);
-    });
-    app.listen(8080);
-  })
-  .catch((error) => console.log(error));
+main();
